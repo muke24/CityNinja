ec8fa085408ee5a9f375b15db3b7f1ca45b37d8d
Peter <pjthompson.au@gmail.com>
2020-02-03 16:28:32 +1100

    More

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void PlayScene()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void QuitGame()
+    {
+
+    }
+
+}
